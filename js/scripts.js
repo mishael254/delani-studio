3c9eedc0b4987a80400f931ae75df0d3537b7482
mishael254 <mishaelmishantoz@gmail.com>
2020-05-17 18:09:14 +0300

    came up with a controlflow system where if the variables were declayed blue,they would be returned

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ var output = function(name, email, message) {
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   var info = ["message has been sent successfuly thankyou for choosing delani studio"]
   var warning = ["invalid response please try again!"]
+  if (name,email,message) {
+    return info
+  } else {
+    return warning
+  }
+}
