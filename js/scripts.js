d8885aecddc7afc0e7b72281b19bb7206a58281a
mishael254 <mishaelmishantoz@gmail.com>
2020-05-17 18:05:58 +0300

    created another variable called warning which also have a string array

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,4 @@
 var output = function(name, email, message) {
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   var info = ["message has been sent successfuly thankyou for choosing delani studio"]
+  var warning = ["invalid response please try again!"]
